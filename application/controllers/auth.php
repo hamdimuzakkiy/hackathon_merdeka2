476471253b6391ff57e24b509f827adfbabbacbb
5112100070 <wahyu12@mhs.if.its.ac.id>
2015-10-24 19:36:10 +0700

    Signup Oke

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -7,7 +7,6 @@ class auth extends CI_Controller {
 		parent::__construct();
 		$this->no_cache();		
 		$this->load->model('user');
-		$this->load->model('wilayah');
 	}
 
 	#ok
@@ -51,27 +50,17 @@ class auth extends CI_Controller {
     }
 
     public function signup(){
-    	if ($this->cek_session())
-			redirect(base_url());
-
-		$list_wilayah = $this->wilayah->get();
-		$list = array();
-		foreach ($list_wilayah as $row) {			
-			$list[$row->id] = $row->nama;
-		}
-		$data['list_wilayah'] = $list;
-    	$this->load->view('auth/signup.php',$data);
+                $this->load->view('auth/signup.php');
     }
 
     public function do_signup(){
     	$data['email'] = $_POST['email'];
     	$data['nama'] = $_POST['nama'];
     	$data['password'] = md5($_POST['password']);
-    	$data['role'] = 'pelapor';
-    	$data['tlp'] = $_POST['tlp'];
-    	$data['id_wilayah'] = $_POST['id_wilayah'];
-    	$this->user->insert($data);
-    	redirect(base_url().'main');
+    	$data['role'] = 'user';
+    	$data['alamat'] = $_POST['alamat'];
+        $this->user->insert($data);
+        redirect(base_url().'main');
     }
 
     #ok
--- database/database.sql
+++ database/database.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v8.6 Beta2
-MySQL - 5.6.14 : Database - monitoring_bencana
+MySQL - 5.6.14 : Database - tanggap_bencana
 *********************************************************************
 */
 
@@ -12,47 +12,56 @@ MySQL - 5.6.14 : Database - monitoring_bencana
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`monitoring_bencana` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`tanggap_bencana` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-USE `monitoring_bencana`;
+USE `tanggap_bencana`;
 
-/*Table structure for table `pasokan` */
+/*Table structure for table `bencana` */
 
-CREATE TABLE `pasokan` (
-  `id_kecamatan` int(11) DEFAULT NULL,
-  `jenis_kebutuhan` varchar(111) DEFAULT NULL,
-  `jumlah` int(11) DEFAULT NULL,
-  `tgl_input` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+CREATE TABLE `bencana` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(111) NOT NULL,
+  `lokasi` varchar(111) DEFAULT NULL,
+  `deskripsi` varchar(222) DEFAULT NULL,
+  `id_user` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `pasokan` */
+/*Data for the table `bencana` */
 
-LOCK TABLES `pasokan` WRITE;
-
-insert  into `pasokan`(`id_kecamatan`,`jenis_kebutuhan`,`jumlah`,`tgl_input`,`id`) values (1,'masker',500,'2015-10-24 11:39:43',1),(1,'makanan balita',500,'2015-10-24 11:40:45',2),(1,'obat',1000,'2015-10-24 11:41:01',3),(1,'tabung oksigen',50,'2015-10-24 11:41:21',4),(2,'masker',100,'2015-10-24 11:41:45',5),(2,'makanan balita',250,'2015-10-24 11:43:03',6),(2,'obat',420,'2015-10-24 11:43:10',7),(3,'tabung oksigen',741,'2015-10-24 11:43:41',8),(3,'masker',1000,'2015-10-24 11:43:50',9);
+LOCK TABLES `bencana` WRITE;
 
 UNLOCK TABLES;
 
-/*Table structure for table `permintaan` */
+/*Table structure for table `kebutuhan` */
 
-CREATE TABLE `permintaan` (
-  `id_kecamatan` int(11) DEFAULT NULL,
-  `jenis_kebutuhan` varchar(111) DEFAULT NULL,
-  `jumlah` int(11) DEFAULT NULL,
+CREATE TABLE `kebutuhan` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `tgl_input` timestamp NULL DEFAULT NULL,
-  `id_user` int(11) DEFAULT NULL,
-  `status` varchar(55) DEFAULT NULL,
+  `nama` varchar(111) NOT NULL,
+  `jumlah` varchar(33) DEFAULT NULL,
+  `id_bencana` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `permintaan` */
+/*Data for the table `kebutuhan` */
 
-LOCK TABLES `permintaan` WRITE;
+LOCK TABLES `kebutuhan` WRITE;
 
-insert  into `permintaan`(`id_kecamatan`,`jenis_kebutuhan`,`jumlah`,`id`,`tgl_input`,`id_user`,`status`) values (1,'masker',550,2,NULL,1,'request'),(1,'makanan balita',100,3,NULL,1,'request'),(1,'obat',1100,4,NULL,1,'request'),(1,'tabung oksigen',50,5,NULL,1,'request'),(1,'air minum',200,6,NULL,1,'request'),(2,'masker',500,7,NULL,1,'request'),(2,'makanan balita',400,8,NULL,1,'request'),(3,'tabung oksigen',1000,9,NULL,1,'request');
+UNLOCK TABLES;
+
+/*Table structure for table `sumbang` */
+
+CREATE TABLE `sumbang` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_kebutuhan` int(11) DEFAULT NULL,
+  `jumlah` int(11) DEFAULT NULL,
+  `bukti` varchar(111) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `sumbang` */
+
+LOCK TABLES `sumbang` WRITE;
 
 UNLOCK TABLES;
 
@@ -61,42 +70,17 @@ UNLOCK TABLES;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nama` varchar(111) DEFAULT NULL,
-  `username` varchar(111) DEFAULT NULL,
-  `password` varchar(111) DEFAULT NULL,
-  `role` varchar(22) DEFAULT NULL,
   `email` varchar(111) DEFAULT NULL,
-  `tlp` varchar(33) DEFAULT NULL,
-  `id_wilayah` int(11) DEFAULT NULL,
+  `password` varchar(111) DEFAULT NULL,
+  `alamat` varchar(111) DEFAULT NULL,
+  `role` varchar(22) DEFAULT NULL,
+  `status` varchar(22) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Data for the table `user` */
 
 LOCK TABLES `user` WRITE;
-
-insert  into `user`(`id`,`nama`,`username`,`password`,`role`,`email`,`tlp`,`id_wilayah`) values (1,'wahyu','123','202cb962ac59075b964b07152d234b70','pelapor','wahyu@pelapor.com',NULL,1);
-
-UNLOCK TABLES;
-
-/*Table structure for table `wilayah` */
-
-CREATE TABLE `wilayah` (
-  `nama` varchar(111) DEFAULT NULL,
-  `koordinat_1` varchar(111) DEFAULT NULL,
-  `koordinat_2` varchar(111) DEFAULT NULL,
-  `cp` varchar(111) DEFAULT NULL,
-  `pic` varchar(111) DEFAULT NULL,
-  `tlp` varchar(111) DEFAULT NULL,
-  `propinsi` varchar(111) DEFAULT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
-/*Data for the table `wilayah` */
-
-LOCK TABLES `wilayah` WRITE;
-
-insert  into `wilayah`(`nama`,`koordinat_1`,`koordinat_2`,`cp`,`pic`,`tlp`,`propinsi`,`id`) values ('kecamatan 1','45.12','12.41','021457412540','hamdi',NULL,'Kalimanatan Timur',1),('kecamatan 2','74.12','124.12','01251241574','kukuh',NULL,'Kalimantan Timur',2),('kecamatan 3','12.147','74.12','02147521441','hanip',NULL,'Kalimantan Timur',3);
 
 UNLOCK TABLES;
 

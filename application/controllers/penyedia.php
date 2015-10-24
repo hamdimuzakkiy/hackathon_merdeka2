bbd83c305269fce91dc5ffe5258f4a66799d289e
hamdi ahmadi muzakkiy <hamdiahmadi1504@gmail.com>
2015-10-24 14:09:46 +0700

    git git

--- application/controllers/penyedia.php
+++ application/controllers/penyedia.php
@@ -1,9 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class grov extends MY_Controller {
+class penyedia extends MY_Controller {
 	
-	public function __construct(){		
+	public function __construct(){
+		$this->laod->model('pasokan');
 		parent::__construct();		
 	}
 

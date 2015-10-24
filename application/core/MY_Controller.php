2d48ee95721e54639a7b1871a00fa11923e55002
5112100070 <wahyu12@mhs.if.its.ac.id>
2015-10-24 13:08:41 +0700

    Pelapor on progress

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -10,8 +10,36 @@ class MY_Controller extends CI_Controller {
 
 	private function no_cache(){
 		header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
-        header("Pragma: no-cache"); // HTTP 1.0.
-        header("Expires: 0"); // Proxies.
+                header("Pragma: no-cache"); // HTTP 1.0.
+                header("Expires: 0"); // Proxies.
 	}
-	
+        
+	protected function cek_session(){
+		if ($this->session->userdata('id') == '')
+		return false;
+		return true;
+	}
+
+	protected function get_session(){
+		$res['role'] = $this->session->userdata('role');
+		$res['id'] = $this->session->userdata('id');
+		$res['nama']= $this->session->userdata('nama');
+		return $res;
+	}
+
+	protected function get_header($data){
+		$res = $this->get_session();		
+		$role = $res['role'];		
+		return $this->load->view('layout/'.$role.'_header.php',$data);
+	}
+
+	protected function get_footer(){
+		$res = $this->get_session();
+		$role = $res['role'];
+		return $this->load->view('layout/'.$role.'_footer.php');
+	}
+
+	protected function get_periode(){
+		return $this->periode->get();
+	}
 }
---
+++ application/views/layout/pelapor_footer.php
@@ -0,0 +1,17 @@
+
+
+    <!-- jQuery 2.1.4 -->
+    
+    <!-- Bootstrap 3.3.5 -->
+    <script src="<?php echo base_url(); ?>../assets/bootsrap/bootstrap/js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src="<?php echo base_url(); ?>../assets/bootsrap/plugins/fastclick/fastclick.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="<?php echo base_url(); ?>../assets/bootsrap/dist/js/app.min.js"></script>
+    
+    <!-- AdminLTE for demo purposes -->
+    <script src="<?php echo base_url(); ?>../assets/bootsrap/dist/js/demo.js"></script>
+    
+  </body>
+</html>
+

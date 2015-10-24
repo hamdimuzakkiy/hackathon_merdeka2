e537c73ef0e1991d76e540db534f032c6394d459
5112100070 <wahyu12@mhs.if.its.ac.id>
2015-10-24 20:16:49 +0700

    Search Function

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -5,9 +5,16 @@ class main extends MY_Controller {
 	
 	public function __construct(){		
 		parent::__construct();
+                $this->load->model('bencana');
 	}
 
 	public function index(){		
 		redirect(base_url().$this->session->userdata('role'));
 	}
+        
+        public function do_search($nama){
+                $data['list_result']=$this->bencana->get_by_name(strtolower($nama));
+                
+                $this->load->view('main/dashboard',$data);
+        }
 }
--- application/views/auth/signup.php
+++ application/views/auth/signup.php
@@ -25,28 +25,28 @@
   <div class="register-box-body">
         <p class="login-box-msg">Daftar Anggota Baru</p>
             <form action="<?php print base_url()?>auth/do_signup" method = "post">
-               <div class="form-group has-feedback">
-                 <input type="text" name = "nama" class="form-control" placeholder = "Nama">
-                 <span class="glyphicon glyphicon-user form-control-feedback"></span>
-               </div>
-               <div class="form-group has-feedback">
-                 <input type="email" class="form-control"  name = "email" placeholder = "Email">
-                 <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-               </div>
-               <div class="form-group has-feedback">
-                 <input type="password" class="form-control" name = "password" placeholder = "Password">
-                 <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-               </div>
-               <div class="form-group has-feedback">
-                 <input type="text" class="form-control" name = "alamat" placeholder = "Alamat">
-                 <span class="glyphicon glyphicon-phone form-control-feedback"></span>
-               </div>
-               <div class="row">
-                 <div class="col-xs-4">
-                   <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
-                 </div><!-- /.col -->
-               </div>
-             </form> 
+          <div class="form-group has-feedback">
+            <input type="text" name = "nama" class="form-control" placeholder = "Nama">
+            <span class="glyphicon glyphicon-user form-control-feedback"></span>
+          </div>
+          <div class="form-group has-feedback">
+            <input type="email" class="form-control"  name = "email" placeholder = "Email">
+            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+          </div>
+          <div class="form-group has-feedback">
+            <input type="password" class="form-control" name = "password" placeholder = "Password">
+            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+          </div>
+          <div class="form-group has-feedback">
+            <input type="text" class="form-control" name = "alamat" placeholder = "Alamat">
+            <span class="glyphicon glyphicon-road form-control-feedback"></span>
+          </div>
+          <div class="row">
+            <div class="col-xs-4">
+              <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
+            </div><!-- /.col -->
+          </div>
+        </form>  
         <a href="<?php echo base_url(); ?>auth/login" class="text-center">Sudah Mendaftar</a>
       </div><!-- /.form-box -->
     </div><!-- /.register-box -->
